b0c897fbe8287182137fd83b0c1c8b52eef3bf19
sgordon007 <sgordon@lbl.gov>
2017-09-07 14:44:24 -0700

    adding Sean Gordon as a new contributor

--- snds.py
+++ snds.py
@@ -5,3 +5,18 @@ def sums(values):
     2, 3]`, the output should be `[9, 12, 3, 6]`.
     """
     pass
+
+def test_empty():
+    assert sums([]) == []
+
+def test_example1():
+    assert sums([1, 2, 3, 3, 1, 5, 6, 3, 1, 2, 3]) == [9, 12, 3, 6]
+
+def test_zero():
+    assert sums([0]) == [0]
+
+def test_single_value():
+    assert sums([5]* 25) == [125]
+
+def test_negative_value():
+    assert sums([-5]* 25) == [125]
