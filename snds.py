046dbf793d38df792c711ebec307418bbc0a76c8
Daniel Standage <daniel.standage@gmail.com>
2017-09-07 14:43:21 -0700

    Update snds.py
    Adding some tests. #TDD #FTW

--- snds.py
+++ snds.py
@@ -8,3 +8,12 @@ def sums(values):
 
 def test_empty():
     assert sums([]) == []
+
+def test_zero():
+    assert sums([0]) == [0]
+
+def test_example():
+    assert sums([1, 2, 3, 3, 1, 5, 6, 3, 1, 2, 3]) == [9, 12, 3, 6]
+
+def test_single_value():
+    assert sums([5] * 25) == [125]
