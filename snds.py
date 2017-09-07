1fb0abaeaf580bfbe5d10c97688f6fc11372364d
Nathan Hillson <njhillson@lbl.gov>
2017-09-07 14:43:05 -0700

    added test for all values decreasing

--- snds.py
+++ snds.py
@@ -2,9 +2,9 @@ def sums(values):
     """
     Given a list of numbers `values`, return a list of the sums of each non-
     decreasing sub-list. For example, if `values = [1, 2, 3, 3, 1, 5, 6, 3, 1,
-    2, 3]`, the output should be `[9, 12, 3, 6]`.
+    2, 3]`, the output should be `[9, 12, 3, 6]`. 
     """
     pass
 
-def test_empty():
-    assert sums([]) == []
+def test_all_values_decreasing():
+    assert sums([9,8,7,6,5,4,3,2,1]) == [9,8,7,6,5,4,3,2,1]
